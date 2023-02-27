60ad10a4ee6d32240ca76f09bae56012ab89e7e6
Daniel <ddvrns@gmail.com>
2023-02-28 02:23:10 +0800

    add cors

--- src/main/java/com/dani/livechatservice/config/SecurityConfig.java
+++ src/main/java/com/dani/livechatservice/config/SecurityConfig.java
@@ -25,7 +25,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
+                .csrf().disable().cors()
+                .and()
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/register", "/authenticate").permitAll()
                         .anyRequest().authenticated())
